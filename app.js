37e095a0b89d496523957e2379ce5b4027df4775
AhmedAli93 <Ahmed98148@gmail.com>
2017-12-29 09:01:12 -0800

    working on it still

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 'use strict'
 
- Image.allImage = [];
- var image = ['img/bag.jpg','img/banana.jpg','img/bathroom.jpg','img/boots.jpg','img/breakfast.jpg','img/bubblegum.jpg','img/chair.jpg','img/cthulhu.jpg','img/dog-duck.jpg','img/dragon.jpg','img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg','img/usb.gif','img/water-can.jpg','img/wine-glass.jpg']
+var past = [];
+var totalClick = 0;
+Image.allImage = [];
+var image = ['img/bag.jpg','img/banana.jpg','img/bathroom.jpg','img/boots.jpg','img/breakfast.jpg','img/bubblegum.jpg','img/chair.jpg','img/cthulhu.jpg','img/dog-duck.jpg','img/dragon.jpg','img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg','img/usb.gif','img/water-can.jpg','img/wine-glass.jpg']
 var imagesToReader = [];
+var random1 = document.getElementById('image1');
+var random2 = document.getElementById('image2');
+var random3 = document.getElementById('image3');
 
 function Image(name, filepath){
   this.name = name;
   this.filepath = filepath;
-  this.clicktotal = 0;
-  this.showntotal = 0;
+  this.clickTotal = 0;
+  this.shownTotal = 0;
   this.id = ''
   this.shown = false;
   Image.allImage.push(this);
@@ -22,14 +27,70 @@ function Image(name, filepath){
 };
 
 function generateThree() {
+
   var counter = 0;
   while(counter < 3){
-    var img = image[Math.floor(Math.random() * 20)];
+    var img = Image.allImage[Math.floor(Math.random() * 20)];
     if(!img.shown){
       imagesToReader.push(img)
       counter++;
       img.shown = true;
-      img.showntotal++;
+      img.shownTotal++;
     }
   }
+
+
+}
+
+function pushPast(){
+  for(var i = 0; i < imagesToReader.length; i++ ){
+    past.push(imagesToReader[i]);
+
+  }
+  console.log(imagesToReader);
+  console.log(past);
+}
+
+function pickPast(){
+  for(var i = 0; i < past.length; i++){
+    if(imagesToReader[i] === past.length[i]){
+
+      generateThree();
+
+        }
+
+    }
+    console.log(imagesToReader);
+    console.log(past);
+}
+
+function create(){
+    random1.setAttribute('src', imagesToReader[0].filepath);
+    random2.setAttribute('src', imagesToReader[1].filepath);
+    random3.setAttribute('src', imagesToReader[2].filepath);
 };
+
+function handleClick(e) {
+  //console.log(e.target.currentSrc.slice(48));
+ console.log(e);
+var selection = e.target.currentSrc.slice(52, -4);
+for (var i = 0; i < Image.allImage.length; i++){
+  if(selection === Image.allImage[i].name){
+    Image.allImage[i].shownTotal++
+  }
+}
+totalClick++;
+
+imagesToReader.length = 0;
+pushPast();
+pickPast();
+generateThree();
+create();
+}
+
+//Call Action
+random1.addEventListener('click', handleClick);
+random2.addEventListener('click', handleClick);
+random3.addEventListener('click', handleClick);
+generateThree();
+create();
